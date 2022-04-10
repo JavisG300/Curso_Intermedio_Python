48ce89c337685718f5d3d2d501dd18fa36d3fcbf
Javier Andrade <javieregonzaleza@gmail.com>
2022-04-10 15:38:32 -0500

    Correción debugging

--- debugging.py
+++ debugging.py
@@ -3,6 +3,7 @@ def divisors(num):
         if num <= 0:
             raise ValueError('Debes ingresar un número positivo')
         divisors = [i for i in range(1, num + 1) if num % i == 0]
+        return divisors
     except ValueError as ve:
         print(ve) 
         return False
