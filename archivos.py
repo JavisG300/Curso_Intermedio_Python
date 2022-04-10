0b3c9552d87f291d6b57f683b48c4e557be06620
Javier Andrade <javieregonzaleza@gmail.com>
2022-04-10 16:11:02 -0500

    clase 19

---
+++ archivos.py
@@ -0,0 +1,12 @@
+def read(): #Funcion que lee un archivo
+    numbers = []
+    with open("./archivos/numbers.txt","r") as f
+
+def write():
+    pass
+
+def run():
+    read()
+
+if __name__ == '__main__':
+    run()
