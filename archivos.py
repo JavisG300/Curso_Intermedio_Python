f004f884b3ac41f68ffea5d06f9f186dba58e232
Javier Andrade <javieregonzaleza@gmail.com>
2022-04-10 21:42:53 -0500

    Funcion read

--- archivos.py
+++ archivos.py
@@ -1,6 +1,9 @@
 def read(): #Funcion que lee un archivo
     numbers = []
-    with open("./archivos/numbers.txt","r") as f
+    with open("./archivos/numbers.txt","r",encoding="utf'8") as f:
+        for line in f: #Leyendo linea por linea del archivo
+            numbers.append(int(line)) #Añadir cada línea con enteros porque python lo lee en string
+        print(numbers)       
 
 def write():
     pass
