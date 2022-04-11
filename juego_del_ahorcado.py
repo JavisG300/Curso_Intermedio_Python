a93ce11526673e8d65d4217da2494f733a6b1985
Javier Andrade <javieregonzaleza@gmail.com>
2022-04-11 09:07:35 -0500

    Juego del ahorcado v2.8

--- juego_del_ahorcado.py
+++ juego_del_ahorcado.py
@@ -30,30 +30,28 @@ def main():
     attemp = 0
     chosen_word = normalize()
     spaces = ["_"] * len(chosen_word)
-    attemps = 5 * len(chosen_word)
-    index = enumerate(chosen_word)
+    attemps = 3 * len(chosen_word)
+    index = dict(enumerate(chosen_word))
     print("Bienvenido al juego del ahorcado: \n")
     print("Adivina la palabra \n")
     print("Tienes " + str(attemps) + " intentos \n")
     for space in spaces:
         print(space, " ", end = "")
     print("\n")    
-    
     while attemp <= attemps:
         letter = input("Escribe una letra ")
         if letter in chosen_word:
-            id = chosen_word.index(letter)
+            id = index[letter]
             space[id] = letter
             print(spaces)
             print("Muy bien!, sigue intentando \n")
-            attemp =+ 1
             print("Te quedan " + str(attemps - attemp) + " intentos")
         else:
             os.system("clear")
             print("Intenta de nuevo \n")
-            attemp =+ 1
             print("Te quedan " + str(attemps - attemp) + " intentos")
             print(spaces)
+        attemp += 1
 
 
 
